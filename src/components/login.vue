7b7d8de31c2d8b88b0a88a177da01135354a7938
hongchangjun <hongcj@dgtis.com>
2018-04-13 17:29:20 +0800

    000-初始结构

---
+++ src/components/login.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="login-box">
+    <div class="top">
+      <el-input
+        placeholder="请输入用户名"
+        v-model="userName"
+        clearable
+        class="user-input">
+        <template slot="prepend">用户名</template>
+      </el-input>
+      <el-input
+        placeholder="请输入密码"
+        v-model="password"
+        clearable
+        type="password"
+        class="user-input">
+        <template slot="prepend">密&nbsp;&nbsp;&nbsp;码</template>
+      </el-input>
+    </div>
+    <div class="bottom">
+      <el-button 
+        type="primary"
+        class="login-btn"
+        v-on:click="login">登&nbsp;&nbsp;&nbsp;录
+      </el-button>
+      <p class="login-tip">
+        <span>登录提示</span>
+        <span class="right">
+          <a href="">没有账号?</a>
+        </span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+function login() {
+  var data = {
+    'userName': this.userName,
+    'password': this.password
+  };
+  this.$http.post('/user/login', data, {emulateJSON: true} ).then(function(result) {
+
+  });
+}
+
+export default {
+  'name': 'login',
+  'data': () => {
+    return {
+      'userName': '',
+      'password': ''
+    }
+  },
+  'methods': {
+    'login': login
+  }
+}
+</script>
+
+<style scoped>
+.login-box {
+  width: 300px;
+  padding: 20px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  box-shadow: 0 0 5px 0 #bbb;
+  margin: 200px auto 0;
+}
+
+.top .user-input {
+  margin-bottom: 20px;
+}
+
+.login-btn {
+  width: 100%;
+  margin-bottom: 20px;
+}
+
+.login-tip {
+  margin: 0;
+  font-size: 12px;
+}
+
+.login-tip .right {
+  float: right;
+}
+</style>
+
+
---
+++ src/main.js
@@ -0,0 +1,20 @@
+
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import App from './App';
+import router from './router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+
+Vue.config.productionTip = false
+
+Vue.use(VueResource);
+Vue.use(ElementUI);
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+});
